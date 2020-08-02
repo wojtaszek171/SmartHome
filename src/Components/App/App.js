1579c3e17aec65756a237f16602f7fe99b92d379
Paweł Wojtaszko <pawelwojtaszko@o2.pl>
2020-08-02 23:16:12 +0200

    Added deploy updates to project

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.scss';
 import Button from '../Button';
 import Header from '../Header';
+import HLSPlayer from 'react-hls';
+
 
 function App() {
+
+  useEffect(() => {
+  
+  }, [])
   return (
     <div className="App">
       <Header />
       <Button text={'Przycisk'} onClick={()=>{console.log('button clicked');}}/>
+      <iframe width="960" height="720" src="http://pwojtaszko.ddns.net:8081"/>
+      <video controls>
+          <source src="http://pwojtaszko.ddns.net:8160/video.avi"/>
+      </video>
+      <HLSPlayer source={'http://pwojtaszko.ddns.net:8160/video.m3u8'} />
     </div>
   );
 }
