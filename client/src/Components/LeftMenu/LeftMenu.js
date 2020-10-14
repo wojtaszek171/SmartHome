082081094e63ba910e2f9c03704cde46a54413be
Paweł Wojtaszko <pawelwojtaszko@o2.pl>
2020-10-15 00:41:22 +0200

    Updated stylings. Added collapsing sidebar and animation for collapse button

--- client/src/Components/LeftMenu/LeftMenu.js
+++ client/src/Components/LeftMenu/LeftMenu.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LeftMenu.scss';
 import TemperatureSection from './TemperatureSection';
 import leftArrow from './assets/arrow_left.svg';
 
 function LeftMenu() {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const handleCollapseClick = () => {
+    setCollapsed(!collapsed);
+  }
 
   return (
-    <div className="left-menu-component">
-      <TemperatureSection />
+    <div className="left-menu-component" >
+      <div className={`left-menu-content${collapsed ? ' sidebar-collapsed' : ' sidebar-expanded'}`}>
+        <TemperatureSection />
+      </div>
       <div className="button-bar">
-        <div className="collapse-button">
-          <img src={leftArrow} />
+        <div className="collapse-button" onClick={handleCollapseClick}>
+          <img src={leftArrow} className={collapsed ? ' sidebar-collapsed' : ' sidebar-expanded'} />
         </div>
       </div>
     </div>
