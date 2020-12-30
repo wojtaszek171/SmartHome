8ffe90fd9f6adcdd1311eedbf64cbf32f3854ea6
Pawel Wojtaszko <pawelwojtaszko@o2.pl>
2020-12-30 22:06:17 +0100

    Added devtools

--- client/src/store.js
+++ client/src/store.js
@@ -1,4 +1,5 @@
 import { createStore } from 'redux';
 import reducers from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 
-export const store = createStore(reducers);
+export const store = createStore(reducers, composeWithDevTools());
