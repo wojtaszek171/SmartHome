39c77e7b3ea64661283b7042387e41b4674fe5e2
Paweł Wojtaszko <pawelwojtaszko@o2.pl>
2022-09-09 17:35:30 +0200

    Disabled eslint for production

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,9 @@
-import { createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { configureStore } from '@reduxjs/toolkit';
+import { isDev } from './helpers';
 import reducers from './reducers';
 
-export const store = createStore(reducers, composeWithDevTools());
+export const store = configureStore({
+    reducer: reducers,
+    middleware: (gDM) => gDM(),
+    devTools: isDev()
+});
